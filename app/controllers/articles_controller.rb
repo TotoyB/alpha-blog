6fd948e5bcafd3beecf5b4bb043903d80ff77b9c
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-25 15:13:33 +0800

    Section 5 eps 97

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :article_id, only:[:show, :destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -19,22 +20,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article has been Successfully removed"
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Update has been successfully"
       redirect_to article_path
@@ -44,6 +41,11 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def article_id
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
