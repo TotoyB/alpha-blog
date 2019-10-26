4588e3c8d93820bce8b1c7c2967de9a985a3021b
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-26 14:10:25 +0800

    added the feature for users sign up

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  resources :users, except: [:new]
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_103956) do
+ActiveRecord::Schema.define(version: 2019_10_26_051832) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_103956) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
