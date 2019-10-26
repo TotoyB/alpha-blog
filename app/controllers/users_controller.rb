9ba6cb08f83ee139894b114ff9edf2e37c1b5c90
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-26 15:56:43 +0800

    viewing of articles by users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :user_id, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @user = User.all
+  end
 
   def new
     @user = User.new()
@@ -15,10 +20,36 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @users_article = @user.articles
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Users account has been updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def desttroy
+
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
 
+  end
+
+  def user_id
+    @user = User.find(params[:id])
   end
 
 end
