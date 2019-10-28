26fed0b304cc3d78b240e927a09996c9176bce95
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-28 19:05:02 +0800

    Section 7 eps 159

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :category, only:[:show, :edit, :update, :destroy]
+  before_action :require_admin, only:[:new, :create, :edit, :update, :destroy]
 
   def index
     @category = Category.paginate(page: params[:page], per_page: 3)
@@ -20,19 +21,22 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
+    if @category.update(category_params)
+      flash[:success] = "You have successfully updated the category name"
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
 
   end
 
   def destroy
-
   end
 
   private
@@ -43,5 +47,12 @@ class CategoriesController < ApplicationController
 
   def category
     @category = Category.find(params[:id])
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = "Only Admin can access this"
+      redirect_to root_path
+    end
   end
 end
