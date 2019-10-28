457a08b6ec2a525c2f3ff72dc77efc95d10bfc46
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-28 14:51:35 +0800

    Done section 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :user_id, only:[:show, :edit, :update, :destroy]
-  before_action :require_user, except:[:index, :show]
+  before_action :require_user, except:[:index, :show, :new, :create]
   before_action :require_same_user, only:[:edit, :update, :destroy]
 
 
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Account created!, Welcome to the Alpha-Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -42,7 +43,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+    if logged_in? && current_user.admin?
+      @user.destroy
+      flash[:danger] = "User and his/her articles, has been deleted"
+      redirect_to users_path
+    end
   end
 
   private
@@ -57,11 +62,9 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != user_id
+    if current_user != user_id && !current_user.admin?
       flash[:danger] = "You cant edit others user info"
       redirect_to users_path
     end
-
   end
-
 end
